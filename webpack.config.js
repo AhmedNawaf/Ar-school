4d53f80f939d829d32d7713ac8653fb959a14416
Ahmed Al-motairi <ahmednawaflearn@gmail.com>
2022-07-23 05:36:25 +0300

    80%

--- webpack.config.js
+++ webpack.config.js
@@ -4,86 +4,90 @@ const path = require('path');
 
 module.exports = {
 
-    entry: './src/js/index.js',
+  entry: './src/js/index.js',
 
-    output: {
-        publicPath: '/',
-        path: path.resolve(__dirname, 'build'),
-        filename: 'js/bundle.js'
-    },
+  output: {
+    publicPath: '/',
+    path: path.resolve(__dirname, 'build'),
+    filename: 'js/bundle.js'
+  },
 
-    module: {
-        rules: [
+  module: {
+    rules: [
+      {
+        test: /\.(sass|css|scss)$/,
+        use: [
           {
-            test: /\.(sass|css|scss)$/,
-            use: [
-              {
-                loader: MiniCssExtractPlugin.loader, 
-                options: {
-                  publicPath: '../'
-                }
-              },
-              "css-loader",
-              "sass-loader",
-            ],
-          },
-
-          {
-            test: /\.(png|jpe?g|gif)$/i,
-            use: [
-              {
-                loader: 'file-loader',
-                options: {
-                  name: '[name].[ext]',
-                  outputPath: "images",
-                }
-              },
-            ],
-          },
-
-          {
-            test: /\.html$/i,
-            loader: 'html-loader',
-          },
-
-          {
-            test: require.resolve("jquery"),
-            loader: "expose-loader",
+            loader: MiniCssExtractPlugin.loader,
             options: {
-              exposes: ["$", "jQuery"],
-            },
+              publicPath: '../'
+            }
+          },
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: "images",
+            }
           },
         ],
-    },
+      },
 
-    performance: {
-      hints: false
-    },
-    
-    devServer: {
-        contentBase: path.join(__dirname, 'build'),
-        // compress: true,
-        port: 9000,
-        writeToDisk: true,
-        stats: 'errors-only',
-        open: true
-    },
-    
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: 'index.html',
-        }),
-        new HtmlWebpackPlugin({
-          template: './src/login.html',
-          filename: 'login.html',
-      }),
-      new HtmlWebpackPlugin({
-        template: './src/sign-up.html',
-        filename: 'sign-up.html',
-    }),
-        new MiniCssExtractPlugin({
-            filename: "css/style.css"
-        })
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+
+      {
+        test: require.resolve("jquery"),
+        loader: "expose-loader",
+        options: {
+          exposes: ["$", "jQuery"],
+        },
+      },
     ],
+  },
+
+  performance: {
+    hints: false
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, 'build'),
+    // compress: true,
+    port: 9000,
+    writeToDisk: true,
+    stats: 'errors-only',
+    open: true
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/login.html',
+      filename: 'login.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/sign-up.html',
+      filename: 'sign-up.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/first-student.html',
+      filename: 'first-student.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/style.css"
+    })
+  ],
 };
